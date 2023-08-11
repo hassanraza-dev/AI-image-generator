818580e89ed25f31f261a2a7401389bb889b02b6
hraza-dev <hassan@elearningavenue.com>
2023-08-11 20:33:01 +0500

    create pages and components

---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreatePost() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">CreatePost</h1>
+    </>
+  );
+}
+
+export default CreatePost;
